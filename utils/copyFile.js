2783513c527d9d2baefee2b9ae4ae087a15d0a26
julyL <849424162@qq.com>
2020-02-21 19:04:02 +0800

    fix bug

--- utils/copyFile.js
+++ utils/copyFile.js
@@ -1,21 +1,12 @@
-const path = require('path');
 const fs = require('fs-extra');
-/**
- * 拷贝fromDir下的filename到toDir
- * @param {String} filename
- * @param {String} fromDir 拷贝目录默认为当前项目最外层目录
- * @param {String} toDir  默认拷贝到process.cwd()
- */
-module.exports = (filename, fromDir, toDir) => {
-  fromDir = fromDir || path.join(__dirname, '../');
-  toDir = toDir || process.cwd();
-  let file = path.join(toDir, filename);
-  if (fs.existsSync(file)) {
-    console.log(`本地已存在，${file}将被重写`);
+
+module.exports = (copyFile, copyToFile) => {
+  if (fs.existsSync(copyToFile)) {
+    console.log(`本地已存在，${copyToFile}将被重写`);
   }
   fs.writeFileSync(
-    file,
-    fs.readFileSync(path.join(fromDir, filename), 'utf-8'),
+    copyToFile,
+    fs.readFileSync(copyFile, 'utf-8'),
     'utf-8'
   );
 };
